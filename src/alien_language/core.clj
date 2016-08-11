c13d000dc7c1567eedadd4813756685586003b79
Horace Williams <horace.d.williams@gmail.com>
2016-08-11 15:00:04 -0600

    make some notes and handle a trivial case

--- src/alien_language/core.clj
+++ src/alien_language/core.clj
@@ -1,6 +1,64 @@
-(ns alien-language.core)
+(ns alien-language.core
+  (:require [clojure.string :refer [split]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn lines [file] (-> file slurp (split #"\n")))
+
+(defn parse-int [string] (Integer/parseInt string))
+
+(defn read-lexicon [file]
+  (loop [lines (->> file lines (drop 1)) ;; dont need the initial line with the count
+         chunks []]
+    (if (empty? lines)
+      chunks
+      (let [count (parse-int (first lines))
+            segment (take count (drop 1 lines))]
+        (recur (drop (inc count) lines)
+               (conj chunks segment))))))
+
+#_(reduce (fn [positions [letter position]]
+            (update positions letter conj position))
+          {}
+          (map vector segment (range (count segment))))
+(defn ordering [segment]
+  (let [positions (zipmap segment (iterate inc 0))
+        ordering (sort-by positions (set segment))]
+    {:status "EXACT" :output (apply str ordering)}))
+;; CASES
+
+;; 1 - Exact
+;; output all letters in the order established by the sample
+;; eg ["a" "b" "c"]
+;; 2 - Ambiguous
+;; output the letters sorted by english
+;; eg ["ce" "he"] => "ceh" (take all unique letters in the segment and sort them)
+;; 3 - Inconsistent / Contradictory
+;; output the letters sorted by english
+
+
+;; 7
+;; 3
+;; z
+;; x
+;; y
+;; 2
+;; ce
+;; he
+;; 3
+;; zc
+;; bc
+;; zd
+;; 2
+;; xy
+;; xyz
+;; 2
+;; hulu
+;; hul
+;; 4
+;; a
+;; a
+;; b
+;; c
+;; 3
+;; e
+;; je
+;; jj
--- test/alien_language/core_test.clj
+++ test/alien_language/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [alien-language.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest reads-an-input
+  (let [segments (read-lexicon "./resources/alien_language_sample.in")]
+    (is (= 7 (count segments)))
+    (is (= [3 2 3 2 2 4 3]
+           (map count segments)))
+    (is (= ["z" "x" "y"]
+           (first segments)))))
+
+(deftest determines-order-for-exact-segment
+  (is (= {:status "EXACT" :output "cba"}
+         (ordering ["c" "b" "a"])))
+  (is (= {:status "EXACT" :output "cba"}
+         (ordering ["c" "b" "a" "a"]))))
